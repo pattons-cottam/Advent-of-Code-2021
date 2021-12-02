b23a712e743f6aadd72cbd1befa11651e6003cb4
pattons-cottam <pattons-cottam@protonmail.com>
2021-12-02 12:16:20 +0000

    day 2 part 1

---
+++ 2. Dive!/Program.cs
@@ -0,0 +1,31 @@
+﻿
+
+PartOne();
+
+void PartOne()
+{
+    int depth = 0, position = 0;
+
+    string[] input = File.ReadAllLines("./puzzle_input.txt");
+
+    foreach (var command in input)
+    {
+        var direction = command.Split(' ')[0];
+        var distance = command.Split(' ')[1];
+
+        switch (direction)
+        {
+            case "forward":
+                position += int.Parse(distance);
+                break;
+            case "up":
+                depth -= int.Parse(distance);
+                break;
+            case "down":
+                depth += int.Parse(distance);
+                break;
+        }
+    }
+
+    Console.WriteLine($"Final position: {position}, final depth: {depth} ({depth * position})");
+}
