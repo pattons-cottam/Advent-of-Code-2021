118af1081ee1ed2b28a92891f906a530f9c22525
pattons-cottam <pattons-cottam@protonmail.com>
2021-12-04 18:12:43 +0000

    day 3 part 2

--- 3. Binary Diagnostic/Program.cs
+++ 3. Binary Diagnostic/Program.cs
@@ -1,6 +1,7 @@
 ﻿var input = File.ReadAllLines("./puzzle_input.txt");
 
 PartOne(input);
+PartTwo(input);
 
 void PartOne(string[] input)
 {
@@ -46,4 +47,55 @@ void PartOne(string[] input)
     Console.WriteLine($"Gamma rate: {gammaRateAsString} ({gammaRate})");
     Console.WriteLine($"Epsilon rate: {epsilonRateAsString} ({epsilonRate})");
     Console.WriteLine($"Power consumption: {gammaRate * epsilonRate}");
+}
+
+void PartTwo(string[] input)
+{
+    var rO2Generator = CalculateRating(input, true);
+    var rO2GeneratorValue = Convert.ToInt32(rO2Generator, 2);
+
+    Console.WriteLine($"Oxygen generator rating: {rO2Generator} ({rO2GeneratorValue})");
+
+    var rCO2Scrubber = CalculateRating(input, false);
+    var rCO2ScrubberValue = Convert.ToInt32(rCO2Scrubber, 2);
+
+    Console.WriteLine($"CO2 scrubber rating: {rCO2Scrubber} ({rCO2ScrubberValue})");
+
+    Console.WriteLine($"Submarine life support rating: {rCO2ScrubberValue * rO2GeneratorValue}");
+}
+
+string CalculateRating(string[] input, bool findMostCommon)
+{
+    var byteSize = input[0].Length;
+    var rating = new int[byteSize];
+    var tempInput = input.ToList();
+
+    for (int i = 1; i <= byteSize; i++)
+    {
+        if (tempInput.Count() == 1) return tempInput.Single();
+
+        var commonValue = FindMostCommonValue(tempInput, i, findMostCommon);
+        tempInput = tempInput
+            .Where(input => input.ToCharArray()[i - 1] == commonValue)
+            .ToList();
+    }
+
+    return tempInput.Single();
+}
+
+char FindMostCommonValue(List<string> input, int bitPosition, bool findMostCommon)
+{
+    var values = new List<int>();
+
+    foreach (var item in input)
+    {
+        values.Add(item.ToCharArray()[bitPosition - 1] - '0');
+    }
+
+    if (findMostCommon)
+    {
+        return values.Sum() >= (double)input.Count / 2 ? '1' : '0';
+    }
+
+    return values.Sum() >= (double)input.Count / 2 ? '0' : '1';
 }
---
+++ 3. Binary Diagnostic/Readme.md
@@ -0,0 +1,13 @@
+### Part One
+```
+Gamma rate: 011011110000 (1776)
+Epsilon rate: 100100001111 (2319)
+Power consumption: 4118544
+```
+
+### Part Two
+```
+Oxygen generator rating: 010111110111 (1527)
+CO2 scrubber rating: 100111001110 (2510)
+Submarine life support rating: 3832770
+```
